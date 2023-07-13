c94d9be7f289fcd3b6e78fc4af7ae36ac42293bc
Kyllian Garcia <kgarcia@z3r10p1.42lyon.fr>
2023-07-13 09:31:58 +0200

    j'ai fini C03

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,24 +6,52 @@
 /*   By: kgarcia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 20:49:22 by kgarcia           #+#    #+#             */
-/*   Updated: 2023/07/12 20:59:26 by kgarcia          ###   ########.fr       */
+/*   Updated: 2023/07/13 09:30:07 by kgarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+#include <stdio.h>
+
+unsigned int	ft_strlen(char *str)
 {
-	unsigned int     i;
-	int     j;
-	unsigned int	len;
+	unsigned int	i;
 
 	i = 0;
-	j = 0;
-	len
-	while (dest[i])
+	while (str[i])
 		i++;
-	while (src[j])
-		dest[i++] = src[j++];
-	dest[i] = '\0';
-	return (dest);
+	return (i);
 }
 
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	srclen;
+	unsigned int	destlen;
+
+	destlen = 0;
+	i = 0;
+	srclen = ft_strlen(src);
+	if (size == 0)
+		return (srclen);
+	while (dest[destlen])
+		destlen++;
+	while (src[i] && destlen + i < (size - 1))
+	{
+		dest[i + destlen] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[destlen + i] = '\0';
+	if (destlen > size)
+		return (srclen + size);
+	return (destlen + srclen);
+}
+/*
+int main(void)
+{
+	char	str1[]= "bien ou quoi";
+	char	str2[]= "wlh je suis zehef";
+	int result = ft_strlcat(str1,str2,1); 
+
+	printf("%d", result);
+}*/
